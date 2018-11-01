c20da4fa5c91d0f7f8e139e1cace924e7bf0483f
Michael Douglas <michaeldouglassd@outlook.com>
2018-11-01 16:47:57 -0300

    meu primeiro php

---
+++ olamundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset=utf-8" />
+<title>Primeiro Exemplo PHP</title>
+<style>
+	h1{
+	color: #0033CC;
+    text-shadow: 1px 1px 1px black;
+    }
+    h2 {
+	color: #0066FF;
+	text-shadow: 1px 1px 1px black;
+    }
+</style>
+</head>
+
+<body>
+	<h1>Aprendendo PHP</h1>
+    <?php
+	  echo "<h2>Olá mundo!</h2>";
+	?>
+</body>
+</html>
